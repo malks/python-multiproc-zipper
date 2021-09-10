2b41b0e5a36df066414adf0eb0a981840288d76b
Osmar Gascho <osmar.malks@gmail.com>
2021-09-10 15:09:44 -0300

    nova opçao para lunender

--- run.py
+++ run.py
@@ -174,7 +174,7 @@ def get_marca_logo(item):
     return logos["fico"]
   elif item["marca"].lower().find('lunender')>=0:
     return logos["lunender"]
-  elif item["marca"].lower().find('LND')>=0:
+  elif item["marca"].lower().find('lnd')>=0:
     return logos["lunender"]
   else:
     return False
